60409ee5f86beaf0c0ad354a6d7ea740ef32622d
asurin <asurin@weddingwire.com>
2012-10-31 16:57:37 -0300

    Update lib/exact_target_sdk/data_extension_object.rb

--- lib/exact_target_sdk/data_extension_object.rb
+++ lib/exact_target_sdk/data_extension_object.rb
@@ -2,6 +2,7 @@ module ExactTargetSDK
 class DataExtensionObject < APIObject
 
   property 'CustomerKey'
+  array_property 'Keys', :nest_children => true
   array_property 'Properties', :nest_children => true
 
 end
